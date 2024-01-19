948a8aec17a5208f8faabd0f4d4dce87f2b2d76c
eizarpaing <eizarpaing@gmail.com>
2024-01-19 17:41:55 +0630

    loops and conditionals

--- exercises/loops.py
+++ exercises/loops.py
@@ -1,4 +1,5 @@
 fruits = ["Apple", "Orange", "Grape"]
+fruits.append("Berry")
 
 # for i in fruits:
 #     print(f'Fruit is : {i}')
@@ -17,14 +18,30 @@ fruits = ["Apple", "Orange", "Grape"]
 #     print(f'Fruit is : {i}')
 # print("hello")
 
+# range(10)
+# range(0, 10)
+
+for i in range(10):
+    print(f'i : {i}')
+
+count = 0  # step 1
+while count < len(fruits):
+    print(f'Fruit is : {fruits[count]}')  # fruits[2]
+    count += 1
 
 # count = 0
-# while count >= len(fruits):
-#   print(f'Fruit is : {fruits[count]}')
-#   count = count + 1
+# while 0<4 => print(fruits[0])
+# count = 0 + 1
+# while 1<4 => print(fruits[1])
+# count = 1 + 1
+# while 2<4 => print(fruits[2])
+# count = 2 + 1
+# while 3<4 => print(fruits[3])
+# count = 3 + 1
+# while 4<4 => quit loop
 
 
-count = 0
-while count < 10:
-    print(f'Count: {count}')
-    count += 1
+# count = 0
+# while count < 10:
+#     print(f'Count: {count}')
+#     count += 1 # count = count + 1
